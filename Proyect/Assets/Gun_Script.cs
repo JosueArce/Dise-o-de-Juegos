0abfc6f5b1e7f10d2a2336d44e8d2d5400c57745
Daniel Montero <d.montero95@gmail.com>
2018-09-02 14:17:40 -0600

    Script para que enemigos persiguan al jugador
    Se le agrega al script el Animator del personaje que debe estar bien hecho y tener estos 3 Booleans:
    -isIdle
    -isWalking
    -isAttacking

---
+++ Proyect/Assets/Gun_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_Script : MonoBehaviour {
+
+	// Use this for initialization
+	public float damage = 10f;
+	public float range = 100f;
+	public Camera fpsCam;
+	public ParticleSystem muzzelFlash;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1")){
+			Shoot();
+		}
+		
+	}
+
+	void Shoot(){
+		muzzelFlash.Play();
+		RaycastHit hit;
+		if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+			Debug.Log(hit.transform.name);
+
+			Enemy enemy = hit.transform.GetComponent<Enemy>();
+			if(enemy != null){
+				enemy.TakeDamage(damage);
+			}
+		}
+	}
+}

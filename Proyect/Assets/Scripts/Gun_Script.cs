fc2af3d8503039ffaeb20c57fd4327826cdbd4d5
Daniel Montero <d.montero95@gmail.com>
2018-09-09 18:25:19 -0600

    ignore

--- Proyect/Assets/Scripts/Gun_Script.cs
+++ Proyect/Assets/Scripts/Gun_Script.cs
@@ -9,17 +9,45 @@ public class Gun_Script : MonoBehaviour {
 	public float impactForce = 30f;
 	public float fireRate = 15f;
 	public float range = 100f;
+
+	public int maxAmmo = 10;
+	private int currentAmmo = -1;
+	public float reloadTime = 2.5f;
+	private bool isReloading = false;
+
 	public Camera fpsCam;
 	public ParticleSystem muzzelFlash;
 	public GameObject bloodFX;
 	public GameObject holeFX;
 
 	private float nextTimeToFire = 0f;
+	public Animator animator;
+
+	void Start(){
+		if(currentAmmo == -1){
+			currentAmmo = maxAmmo;
+		}
+	}
+
+	void OnEnable(){
+		isReloading = false;
+		animator.SetBool("reloading",false);
+	}
 
     // Update is called once per frame
     void Update () {
+		if(isReloading){
+			return;
+		}
+		animator.SetBool("idle",true);
+		if(currentAmmo <= 0){
+			animator.SetBool("idle",false);
+			StartCoroutine(Reload());
+			return;
+		}
 
 		if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire){
+			animator.SetBool("idle",false);
 			nextTimeToFire = Time.time + 1f/fireRate;
 			Shoot();
 		}
@@ -28,7 +56,9 @@ public class Gun_Script : MonoBehaviour {
 
 	void Shoot(){
 		muzzelFlash.Play();
+		currentAmmo --;
 		RaycastHit hit;
+		animator.SetBool("shooting",true);
 		if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
 			Debug.Log(hit.transform.name);
 
@@ -36,16 +66,29 @@ public class Gun_Script : MonoBehaviour {
 			if(enemy != null){
 				enemy.TakeDamage(damage);
 			}
-
 			if(hit.rigidbody != null){
 				hit.rigidbody.AddForce(-hit.normal * impactForce);
 			}
-			if(hit.transform.tag=="Dummie")
+			if(hit.transform.tag=="Dummie"){
 				Instantiate(bloodFX, hit.point, Quaternion.LookRotation(hit.normal));
-			if(hit.transform.tag=="LevelPart")
+			}
+			if(hit.transform.tag=="LevelPart"){
 				Instantiate(holeFX, hit.point, Quaternion.LookRotation(hit.normal));
+			}	
+			animator.SetBool("shooting",false);
+			animator.SetBool("idle",true);
 		}
+		animator.SetBool("shooting",false);
+		animator.SetBool("idle",true);
+	}
 
-			
+	IEnumerator Reload(){
+		isReloading = true;
+		animator.SetBool("reloading",true);
+		yield return new WaitForSeconds(reloadTime -0.25f);
+		animator.SetBool("reloading",false);
+		yield return new WaitForSeconds(0.25f);
+		currentAmmo = maxAmmo;
+		isReloading = false;
 	}
 }
